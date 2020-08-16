fc5bb42c338b608d6a92860e30ec568cdebc8018
Douglas de León <dele18037@uvg.edu.gt>
2020-08-15 23:26:12 -0600

    cambios?

--- Obj/Obj.cpp
+++ Obj/Obj.cpp
@@ -1,135 +1,85 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <cstdlib>
 #include <string>
 #include <deque>
-#include <vector>
-#include "../structures/structures.h"
 #include "Obj.h"
-#include "../Render/Render.h"
 
 #define LOG(x) std::cout << x << std::endl
-
-std::vector<std::string> Obj::split (const std::string &s, char delim) {
-    std::vector<std::string> result;
-    std::stringstream ss (s);
-    std::string item;
-
-    while (std::getline (ss, item, delim)) {
-        result.push_back (item);
-    }
-
-    return result;
-}
-
-std::vector<int> Obj::split_faces(std::string s, char delim) {
-    std::vector<int> result;
-    std::string item = "";
-
-
-    for ( std::string::iterator it=s.begin(); it!=s.end(); ++it) {
-        if (*it == delim) {
-            try {
-                int token = std::stoi(item);
-                result.push_back(token);
-                item = "";
-            }
-            catch(std::invalid_argument& e){
-            // if no conversion could be performed
-                item = "";
-            }
-        }
-        else {
-            item += *it;
-        }
-    }
-
-    try {
-        int token = std::stoi(item);
-        result.push_back(token);
-    }
-    catch(std::invalid_argument& e){
-    // if no conversion could be performed
-    }
-    return result;
-}
 
 Obj::Obj(std::string filename) {
     std::ifstream file(filename);
     std::string line;
-    
+
     // Get all lines into the deque
     while(std::getline(file, line))
         lines.push_back(line);
 
-    file.close();
-
     // Split lines in words.
     for (auto &&line: lines) {
-        //Separating line into words.
         std::deque<std::string> curr_line;
-        std::stringstream ss (line);
-        std::string item;
+        std::string word = "";
 
-        while (std::getline (ss, item, ' ')) {
-            curr_line.push_back (item);
+        for (auto &&x: line) {
+            if (line.at(0) == '#')
+                break;
+            if (x == ' ') {
+                curr_line.push_back(word);
+                word = "";
+            }
+            else
+                word += x;
         }
 
-        // Add to structure depending on line prefix.
         if (!curr_line.empty()) {
             std::string prefix = curr_line.front();
             curr_line.pop_front();
 
-            // Vertices
             if (prefix == "v") {
-                std::vector<double> vertex;
+                std::deque<float> vertex;
+                for (auto &&word: curr_line) 
+                    vertex.push_back(std::stof(word));
 
-                for (auto &&word: curr_line) {
-                    vertex.push_back(std::stod(word));
-                }
-                V3 vert;
-                if (vertex.size() == 3) {
-                    vert.x = vertex[0];
-                    vert.y = vertex[1];
-                    vert.z = vertex[2];
-                }
-                else {
-                    vert.x = vertex[0];
-                    vert.y = vertex[1];
-                    vert.z = 0.0;
-                }
-                vertices.push_back(vert);
+                vertices.push_back(vertex);
 
-            // Normals
             } else if (prefix == "vn") {
-                std::vector<double> normal;
+                std::deque<float> normal;
                 for (auto &&word: curr_line)
-                    normal.push_back(std::stod(word));
+                    normal.push_back(std::stof(word));
 
                 normals.push_back(normal);
 
-            // Textures
             } else if (prefix == "vt") {
-                std::vector<double> texture;
+                std::deque<float> texture;
                 for (auto &&word: curr_line)
-                    texture.push_back(std::stod(word));
+                    texture.push_back(std::stof(word));
 
                 texcoords.push_back(texture);
 
-            //Faces
             } else if (prefix == "f") {
-                std::vector<std::vector<int> > face;
+                std::deque< std::deque<int> > face;
                 for (auto &&word: curr_line) {
-                    std::vector<int> vals = split_faces(word, '/');
+                    std::deque<int> vals;
+                    std::string str = "";
+
+                    for (auto &&x: word) {
+                        if (x == '/') {
+                            vals.push_back(std::stoi(str));
+                            str = "";
+                        }
+                        else
+                            str += x;
+                    }
                     face.push_back(vals);
                 }
-
                 faces.push_back(face);
-                
             }
         }
     }
+
+    file.close();
 }
 
 Obj::~Obj() {}
+
+void Obj::read() {}
--- Obj/Obj.h
+++ Obj/Obj.h
@@ -4,22 +4,18 @@
 #include <deque>
 #include <vector>
 #include <string>
-#include "../structures/structures.h"
-#include "../Render/Render.h"
 
 class Obj {
 private:
     std::deque<std::string> lines;
 
 public:
-    std::vector<V3> vertices;
-    std::vector<std::vector<double> > normals;
-    std::vector<std::vector<double> > texcoords;
-    std::vector<std::vector< std::vector<int> > > faces;
+    std::deque< std::deque<float> > vertices;
+    std::deque< std::deque<float> > normals;
+    std::deque< std::deque<float> > texcoords;
+    std::deque< std::deque< std::deque<int> > > faces;
     Obj(std::string filename);
     ~Obj();
 
-    std::vector<std::string> split(const std::string &s, char delim);
-
-    std::vector<int> split_faces(std::string s, char delim);
-};
+    void read();
+}; 
